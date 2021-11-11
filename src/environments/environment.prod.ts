7c834ab43e289e5a23120341a0427d8de55cedff
OlyaPinchuk <olha5959@gmail.com>
2021-11-11 16:49:14 +0200

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   API: 'api/v1',
-  API_HOST: 'http://localhost:8000'
+  API_HOST: 'http://django-env.eba-tfm4buue.us-west-2.elasticbeanstalk.com/'
 };

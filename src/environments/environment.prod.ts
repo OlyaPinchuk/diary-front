fd8ef24fad8b6c37b1f3d72771fa2c3fb6aa1541
OlyaPinchuk <olha5959@gmail.com>
2021-11-11 17:00:22 +0200

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   API: 'api/v1',
-  API_HOST: ' https://cors-everywhere.herokuapp.com/http://django-env.eba-tfm4buue.us-west-2.elasticbeanstalk.com/'
+  API_HOST: ' https://cors-everywhere.herokuapp.com/http://django-env.eba-tfm4buue.us-west-2.elasticbeanstalk.com'
 };
